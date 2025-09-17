db515893d7c35b047dffd609a4cc1987a78a0c5a
suryansh0010 <suryanshawasthi9@gmail.com>
2025-09-17 05:39:30 +0530

    meww

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import AuthPage from "./pages/auth";
+import AuthPage from "./pages/auth"; 
 
 const queryClient = new QueryClient();
 
